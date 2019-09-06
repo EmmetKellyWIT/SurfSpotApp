bcc4bfdd5be5295d28366c5ac3462c6e968e9ab7
EmmetKellyWIT <20075740@mail.wit.ie>
2019-09-05 19:13:32 -0700

    Commiting and pushing files

--- app/src/main/java/com/project/surfspotapp/models/SurfSpotModel.kt
+++ app/src/main/java/com/project/surfspotapp/models/SurfSpotModel.kt
@@ -4,6 +4,8 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 
+
+
 @Parcelize
 data class SurfSpotModel (var id: Long =0,
                            var title: String = "",
